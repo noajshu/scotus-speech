34a4c0cd7a288f2dfc9edba042b06859c1b85d6b
Noah Shutty <noahshutty@gmail.com>
2019-01-06 15:42:50 -0800

    convert corpus to Kur format with no numbers / symbols

--- convert_corpus.py
+++ convert_corpus.py
@@ -4,19 +4,34 @@ import os
 import shutil
 
 
+def strip_non_alphanum(string):
+    return ''.join(
+        c
+        for c in string
+        if c.isalnum() or c == ' ' or c == "'"
+    )
+
+def strip_non_ascii(string):
+    return ''.join(c for c in string if 0 < ord(c) < 127)
+
 print('converting to KUR format')
 with jsonl.JRZ('corpus_staging/corpus.utterances.jsonl.gz') as infile:
     os.system('mkdir -p outputs/kur/audio')
     with jsonl.JW('outputs/kur/scotusspeech.jsonl') as outfile:
         for utterance in infile:
             utterance_id = str(uuid())
+            shutil.copyfile(
+                'audio/{}/{}'.format(utterance['utterance_dir'], utterance['utterance_file']),
+                'outputs/kur/audio/{}.mp3'.format(utterance_id)
+            )
+            if any(c.isdigit() for c in utterance['text']):
+                continue
+            utterance['text'].lower()
             outfile.dump({
                 'uuid': utterance_id ,
                 'duration_s': round(float(utterance['duration']), 3),
                 # leave punctuation for now
-                'text': utterance['text'].lower()
+                'text': ' '.join(strip_non_ascii(strip_non_alphanum(
+                    utterance['text'].lower()
+                )).split())
             })
-            shutil.copyfile(
-                'audio/{}/{}'.format(utterance['utterance_dir'], utterance_file),
-                'outputs/kur/audio/{}.mp3'.format(utterance_id)
-            )
