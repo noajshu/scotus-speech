41a9b7abd93d7730b6d1979f4fd1a5f0950e1e8c
Noah Shutty <noahshutty@gmail.com>
2019-01-06 17:43:51 -0800

    remove experimental .wav support from play_alignment

--- play_alignment.sh
+++ play_alignment.sh
@@ -2,7 +2,7 @@ PREFIX=$1 # e.g., downloads/2010/10-5400
 jq -c '.fragments[]' $PREFIX.pdf.utts.txt.map.json | head -n300 | tail -n24 | while read -r fragment; do
     echo $(jq -r '.lines[0]' <<< $fragment) # | jq -r '.lines[0]'
     echo duration $(echo $(jq -r '.end' <<< $fragment) - $(jq -r '.begin' <<< $fragment) | bc)s
-    play -q $PREFIX.mp3.wav trim $(jq -r '.begin' <<< $fragment) =$(jq -r '.end' <<< $fragment)
+    play -q $PREFIX.mp3 trim $(jq -r '.begin' <<< $fragment) =$(jq -r '.end' <<< $fragment)
     sleep 0.25
 done
 
