5ed48def331fc77cf36ec8cd0780008f044d8083
Noah Shutty <noahshutty@gmail.com>
2019-01-06 16:08:39 -0800

    transcoding cuts bash script

--- cut_audio.sh
+++ cut_audio.sh
@@ -1,4 +1,4 @@
-gunzip -c corpus_staging/corpus.utterances.jsonl.gz | head -n100 | while read -r line; do
+gunzip -c corpus_staging/corpus.utterances.jsonl.gz | while read -r line; do
     mkdir -p audio/$(jq -r '.utterance_dir' <<< $line)/
     echo ffmpeg -y -i $(jq -r '.audiofile' <<< $line) \
         -ss $(jq -r '.start' <<< $line) \
