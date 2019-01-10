c279be614d78ecc54578be26c201d29bb4d74c65
Noah Shutty <noahshutty@gmail.com>
2019-01-10 10:06:52 -0800

    escape case directory paths in cut_audio.sh

--- cut_audio.sh
+++ cut_audio.sh
@@ -1,6 +1,6 @@
 gunzip -c corpus_staging/corpus.utterances.jsonl.gz | while read -r line; do
     mkdir -p audio/$(jq -r '.utterance_dir' <<< $line)/
-    echo ffmpeg -y -i $(jq -r '.audiofile' <<< $line) \
+    echo ffmpeg -y -i \"$(jq -r '.audiofile' <<< $line)\" \
         -ss $(jq -r '.start' <<< $line) \
         -t $(jq -r '.duration' <<< $line) \
         audio/$(jq -r '.utterance_dir' <<< $line)/$(jq -r '.utterance_file' <<< $line)
