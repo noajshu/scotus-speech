d9bda216a1b8ad849c00cd71b10279f19fd7804c
Noah Shutty <noahshutty@gmail.com>
2019-06-08 10:52:37 -0700

    enable pdf GETs in download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 gunzip -c corpus_staging/manifest.jsonl.gz | while read -r line; do    
     echo wget -N --retry-connrefused --waitretry=100 --read-timeout=20 --timeout=15 -t 0 -O \"$(jq -r '.audio_mp3_path' <<< $line)\" \"$(jq -r '.audio_mp3_url' <<< $line)\"
-    #echo wget -O \"$(jq -r '.transcript_pdf_path' <<< $line)\" \"$(jq -r '.transcript_pdf_url' <<< $line)\"
+    echo wget -O \"$(jq -r '.transcript_pdf_path' <<< $line)\" \"$(jq -r '.transcript_pdf_url' <<< $line)\"
 done | parallel
